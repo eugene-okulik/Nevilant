288d65d3b8aeb5420800e125a1a3c3cda459cae4
Nevilant <valentin94.94@mail.ru>
2024-03-16 15:03:49 +0300

    Changed: applied the index method to all

--- homework/valentin_lebedev/Homework_5/ex.py
+++ homework/valentin_lebedev/Homework_5/ex.py
@@ -9,11 +9,13 @@ a, b, c, d, e = person
 text_1 = 'результат операции: 42'
 text_2 = 'результат операции: 514'
 text_3 = 'результат работы программы: 9'
-slice_text_1 = int(text_1[-2:]) + 10
-slice_text_2 = int(text_2[-3:]) + 10
-slice_and_index_text_3 = int(text_3[text_3.index(':') + 2:]) + 10
 
-print(slice_text_1, slice_text_2, slice_and_index_text_3, sep='\n')
+
+def slice_text(text):
+    return int(text[text.index(':') + 2:]) + 10
+
+
+print(slice_text(text_1) + 10, slice_text(text_2), slice_text(text_3), sep='\n')
 
 # Задание 3
 
