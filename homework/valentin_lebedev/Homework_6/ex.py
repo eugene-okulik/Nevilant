338ffbd28f2e22f789a1491a5d0827225425e18f
Nevilant <valentin94.94@mail.ru>
2024-03-17 23:38:58 +0300

    Update: edited line breaks

--- homework/valentin_lebedev/Homework_6/ex.py
+++ homework/valentin_lebedev/Homework_6/ex.py
@@ -1,9 +1,7 @@
 # Задание 1
 
-text = (
-        "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
-        + "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
-)
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+        + "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 list_text = text.split()
 edit_text = []
 
