8ff9c844b3fc88ae0d4c5a1a560d4b6438c7c9ba
Nevilant <valentin94.94@mail.ru>
2024-10-08 23:44:27 +0300

    homework_19 corrected

--- homework/valentin_lebedev/Homework_19/test_ex.py
+++ homework/valentin_lebedev/Homework_19/test_ex.py
@@ -17,7 +17,8 @@ def before_after():
 
 
 @pytest.fixture()
-def create_post():
+def pre_and_post_conditions():
+    """Пре и пост условия"""
     body = {
         "name": "Holidays",
         "data":
@@ -42,7 +43,33 @@ def create_post():
     requests.delete(url=f"http://167.172.172.115:52353/object/{post_id}")
 
 
-def test_put_post(create_post, before_after):
+@pytest.fixture()
+def pre_condition():
+    """Предусловие"""
+    body = {
+        "name": "Holidays",
+        "data":
+            {
+                "January": ["New Year Holidays", "Christmas Day"],
+                "February": ["Defender of the Fatherland Day"],
+                "March": ["International Women’s Day"],
+                "May": ["Spring and Labour Holiday", "Victory Day"],
+                "June": ["Day of Russia"],
+                "November": ["National Unity Day"],
+            }
+    }
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.post(url="http://167.172.172.115:52353/object", json=body, headers=headers)
+
+    post_id = response.json()["id"]
+    print("Post ID:", post_id)
+
+    yield post_id
+
+
+def test_edit_object_with_put_method(pre_and_post_conditions, before_after):
+    """Редактирование объекта с помощью метода PUT"""
     body = {
         "name": "Holidays",
         "data":
@@ -63,7 +90,7 @@ def test_put_post(create_post, before_after):
     headers = {'Content-Type': 'application/json'}
 
     response = requests.put(
-        url=f"http://167.172.172.115:52353/object/{create_post}",
+        url=f"http://167.172.172.115:52353/object/{pre_and_post_conditions}",
         json=body,
         headers=headers
     )
@@ -71,7 +98,8 @@ def test_put_post(create_post, before_after):
     assert len(response.json()["data"]) == 11
 
 
-def test_patch_post(create_post, before_after):
+def test_edit_object_with_patch_method(pre_and_post_conditions, before_after):
+    """Редактирование объекта с помощью метода PATCH"""
     body = {
         "name": "Holidays",
         "data":
@@ -92,25 +120,27 @@ def test_patch_post(create_post, before_after):
     headers = {'Content-Type': 'application/json'}
 
     response = requests.patch(
-        url=f"http://167.172.172.115:52353/object/{create_post}",
+        url=f"http://167.172.172.115:52353/object/{pre_and_post_conditions}",
         json=body,
         headers=headers
     )
     assert response.json()["data"]["April"] == "There are no holidays"
 
 
-def test_get_post(create_post, before_after):
+def test_get_object(pre_and_post_conditions, before_after):
+    """Получение объекта с помощью метода GET"""
     headers = {'Content-Type': 'application/json'}
 
     response = requests.get(
-        url=f"http://167.172.172.115:52353/object/{create_post}",
+        url=f"http://167.172.172.115:52353/object/{pre_and_post_conditions}",
         headers=headers
     )
     assert response.status_code == 200, "Status code should be 200"
 
 
 @pytest.mark.parametrize("name", ["Holidays", "Vacations", "Celebrations"])
-def test_create_post(before_after, name):
+def test_create_object(before_after, name):
+    """Создание объекта с помощью метода POST"""
     body = {
         "name": name,
         "data": {
@@ -122,3 +152,9 @@ def test_create_post(before_after, name):
     response = requests.post(url="http://167.172.172.115:52353/object", json=body, headers=headers)
     assert response.status_code == 200
     assert response.json()["name"] == name
+
+
+def test_delete_object(before_after, pre_condition):
+    """Удаление объекта с помощью метода delete"""
+    response = requests.delete(url=f"http://167.172.172.115:52353/object/{pre_condition}")
+    assert response.status_code == 200
